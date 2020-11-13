c01b798157dc97fadc6b66a00baa84c18f59cd10
Filipe Silva <filipesilva.biz@gmail.com>
2020-11-13 16:18:27 +0000

    Switched to Barista library (Espresso wrapper), added member sort test

--- app/src/androidTest/java/com/codewarsclient/MembersTest.kt
+++ app/src/androidTest/java/com/codewarsclient/MembersTest.kt
@@ -1,17 +1,20 @@
 package com.codewarsclient
 
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withHint
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.schibsted.spain.barista.assertion.BaristaListAssertions.assertDisplayedAtPosition
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import com.schibsted.spain.barista.rule.cleardata.ClearDatabaseRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -19,6 +22,9 @@ class MembersTest {
 
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @get:Rule
+    val clearDatabaseRule = ClearDatabaseRule()
 
     @Test
     fun assertMemberSearchHint() {
@@ -36,12 +42,32 @@ class MembersTest {
 
         Thread.sleep(3000)
 
-        onView(withId(R.id.list_of_members))
-            .perform(
-                RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
-                    hasDescendant(withText(memberUsername))
-                )
-            )
+        assertDisplayedAtPosition(R.id.list_of_members, 0, R.id.text_name, memberUsername)
+    }
+
+    @Test
+    fun assertMemberSortFunctions() {
+        val memberUsernames = arrayOf("myjinxin2015", "qwe")
+
+        memberUsernames.onEach {
+            onView(withId(R.id.input_text_member_name))
+                .perform(click())
+                .perform(clearText())
+                .perform(typeText(it))
+                .perform(pressImeActionButton())
+            Thread.sleep(3000)
+        }
+
+        // Asset sort by time of search desc
+        assertDisplayedAtPosition(R.id.list_of_members, 0, R.id.text_name, memberUsernames[1])
+        assertDisplayedAtPosition(R.id.list_of_members, 1, R.id.text_name, memberUsernames[0])
+
+        // Press the sort by rank button
+        clickOn(R.id.sort_by_rank)
+
+        // Asset sort by rank desc
+        assertDisplayedAtPosition(R.id.list_of_members, 0, R.id.text_name, memberUsernames[0])
+        assertDisplayedAtPosition(R.id.list_of_members, 1, R.id.text_name, memberUsernames[1])
     }
 
 }
