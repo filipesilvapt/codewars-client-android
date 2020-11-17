264d80d8f787d0d54d698daa70df20fa3661e5ba
Filipe Silva <filipesilva.biz@gmail.com>
2020-11-17 00:39:56 +0000

    Only show end of list reached if the list has items

--- app/src/main/java/com/codewarsclient/ui/challenges/completed/CompletedChallengesViewModel.kt
+++ app/src/main/java/com/codewarsclient/ui/challenges/completed/CompletedChallengesViewModel.kt
@@ -45,7 +45,8 @@ class CompletedChallengesViewModel @ViewModelInject constructor(
                 if (completedChallengesWrapper.wasObtainedFromApi) pageNumber++
 
                 challengesListAdapter.appendToChallengesList(completedChallengesWrapper.challengesList)
-            } else {
+            } else if (challengesListAdapter.itemCount > 0) {
+                // Only show end of list reached if the list has at least one item
                 setEndOfListReachedStatus()
             }
         }
